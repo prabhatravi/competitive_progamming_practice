4b543285c181ca024b7ccd4ef8f546883697d07c
Prabhat Ravi <prabhat.ravi.engg@gmail.com>
2022-08-06 20:53:44 +0530

    Prime number using Seive algorithm

---
+++ prime_number.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+const int N=1e5+5;
+vector<int>prime;
+vector<int>ls;
+void seive(){
+    for (int i=2;i<N;i++) {
+        if (prime[i]) {
+            ls.push_back(i);
+            for (int j=i*2;j<N;j=+i) {
+                prime[j] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    prime.resize(N, 1);
+    prime[0]=prime[1]=0;
+    seive();
+    for(auto e:ls){
+        cout<<e<<" ";
+    }
+}
