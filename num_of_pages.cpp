4810799769cae30c5f47a3d9c77f1585429fb855
Prabhat Ravi <prabhat.ravi.engg@gmail.com>
2022-08-07 20:28:05 +0530

    num of pages

---
+++ num_of_pages.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    bool ck(long long int x, int a[], int n, int m) {
+        int cn=0;
+        long long int sum=0;
+        for(int i=0; i<n, i++) {
+            if (sum+a[i] <= x) {
+                sum += a[i];
+            } else {
+                cn++;
+                sum =0;
+                if (sum+a[i]<= x) {
+                    sum += a[i];
+
+                } else {
+                    return 0;
+                }
+            }
+        }
+    }
+};
