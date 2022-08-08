cb7d82d04d3e66711a7222ddd0e332730128ee90
Prabhat Ravi <prabhat.ravi.engg@gmail.com>
2022-08-08 20:21:27 +0530

    fix the typo

--- num_of_pages.cpp
+++ num_of_pages.cpp
@@ -6,7 +6,7 @@ class Solution {
     bool ck(long long int x, int a[], int n, int m) {
         int cn=0;
         long long int sum=0;
-        for(int i=0; i<n, i++) {
+        for(int i=0; i<n; i++) {
             if (sum+a[i] <= x) {
                 sum += a[i];
             } else {
