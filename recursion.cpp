7b2c0a452f592c7aaf92cd415d86e07de0d9babc
Prabhat Ravi <prabhat.ravi.engg@gmail.com>
2022-08-14 23:05:15 +0530

    knack program

---
+++ recursion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int rec(int ind, int w) {
+    if (ind == n || w == 0) {
+        return 0;
+    }
+ 
+    int leave=rec(ind+1, w);
+    if (weight[ind]<=w)
+        int take=rec(ind+1, w-weignt[ind])+val[ind];
+    return max(take, leave);
+}
