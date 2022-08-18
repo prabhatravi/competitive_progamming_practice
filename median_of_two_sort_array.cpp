6d88f17ed1e44d6600799775446eeaa90e87cc17
Prabhat Ravi <prabhat.ravi.engg@gmail.com>
2022-08-18 22:04:24 +0530

    fix the typo

--- median_of_two_sort_array.cpp
+++ median_of_two_sort_array.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-float median(int num 1[],int num2[],int m,int n) {
+float median(int num1[],int num2[],int m,int n) {
     if(m>n)
-        return median(nums2,nums1,n,m);//ensuring that binary search happens on minimum size array
+        return median(num2,num1,n,m);//ensuring that binary search happens on minimum size array
         
     int low=0,high=m,medianPos=((m+n)+1)/2;
     while(low<=high) {
         int cut1 = (low+high)>>1;
         int cut2 = medianPos - cut1;
         
-        int l1 = (cut1 == 0)? INT_MIN:nums1[cut1-1];
-        int l2 = (cut2 == 0)? INT_MIN:nums2[cut2-1];
-        int r1 = (cut1 == m)? INT_MAX:nums1[cut1];
-        int r2 = (cut2 == n)? INT_MAX:nums2[cut2];
+        int l1 = (cut1 == 0)? INT_MIN:num1[cut1-1];
+        int l2 = (cut2 == 0)? INT_MIN:num2[cut2-1];
+        int r1 = (cut1 == m)? INT_MAX:num1[cut1];
+        int r2 = (cut2 == n)? INT_MAX:num2[cut2];
         
         if(l1<=r2 && l2<=r1) {
             if((m+n)%2 != 0)
